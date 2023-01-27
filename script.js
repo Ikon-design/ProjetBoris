0fd96345ed4a603e2e9a8b0d8fa80e48560f0c41
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 12:13:16 +0100

    URL

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ let text = document.getElementById("title")
 let rick = document.getElementById("rick")
 
 let tablele = ["left", "center", "right"]
-console.log("rz")
-function lancezMoi(){
-    let sound = new Audio("./sound.mp3")
-    sound.play()
 
+function lancezMoi(){
+    let sound = new Audio("sound.mp3")
+    sound.play()
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.floor(Math.random() * 3)

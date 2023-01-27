1afaed44525a5f2e92225a8a37254d379e121741
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 11:38:45 +0100

    mouv your body

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 let container = document.getElementById("container")
+let text = document.getElementById("title")
+let rick = document.getElementById("rick")
+
+let tablele = ["left", "center", "right"]
 
 function lancezMoi(){
-    console.log("coucou")
+    text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+    let matchrer = Math.random(3)
+    container.style.textAlign = tablele[matchrer]
 }
 
 setInterval(lancezMoi, 100)

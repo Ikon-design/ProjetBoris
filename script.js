31347dbff14fc3cdd5affe684f1d9e8dc1c2482e
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 11:40:35 +0100

    test

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function lancezMoi(){
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.random(3)
-    console.log(tablele[matchrer])
+    console.log(tablele[1])
     container.style.textAlign = tablele[matchrer]
 }
 

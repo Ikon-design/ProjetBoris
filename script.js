6126102fb808f56d549e7d48e55e2f6af0453889
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 12:11:30 +0100

    test

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ let rick = document.getElementById("rick")
 let tablele = ["left", "center", "right"]
 
 function lancezMoi(){
-    let sound = document.createElement("audio")
-    sound.src = "./sound.mp3"
+    let sound = new Audio("./sound.mp3")
+    sound.play()
+
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.floor(Math.random() * 3)
     container.style.textAlign = tablele[matchrer]
-    console.log("Rotate(" + Math.floor(Math.random() * 360) + "deg)")
     rick.style.transform = "Rotate(" + Math.floor(Math.random() * 360) + "deg)"
     text.style.fontSize = Math.floor(Math.random() * 250) + "px"
-    document.body.insertBefore(sound, rick)
 }
 
 setInterval(lancezMoi, 100)

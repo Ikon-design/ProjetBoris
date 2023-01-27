f2fbe11836d48313e668a80d174668db27ca68a9
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 12:54:51 +0100

    test

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ console.log(tablele)
 function lancezMoi(){
     let sound = new Audio("sound.mp3")
     sound.play()
-    playSound()
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.floor(Math.random() * 3)
@@ -15,13 +14,5 @@ function lancezMoi(){
     rick.style.transform = "Rotate(" + Math.floor(Math.random() * 360) + "deg)"
     text.style.fontSize = Math.floor(Math.random() * 250) + "px"
 }
-
-
-function playSound(){
-    let sound = new Audio("sound.mp3")
-    sound.play()
-    setTimeout(sound.pause(), 5000)
-}
-
 
 setInterval(lancezMoi, 1000)

cd031eb26a194d6c21179ab3d1ff2228b4e62cea
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 11:28:24 +0100

    changement de couleur

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let container = document.getElementById("container")
 
+function lancezMoi(){
+    container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+}
+
+setTimeout(lancezMoi, 1000)

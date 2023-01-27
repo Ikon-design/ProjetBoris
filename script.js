f890c665eec01da124a15aaf172c8f1b868eae85
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 11:44:35 +0100

    test

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let tablele = ["left", "center", "right"]
 function lancezMoi(){
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
-    let matchrer = Math.random(3)
+    let matchrer = Math.floor(Math.random(3))
     console.log(matchrer)
     console.log(tablele)
     console.log(tablele[matchrer])

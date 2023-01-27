09ff2372aa4e1f2c4a69ffed489d8e3829d9c626
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 11:41:31 +0100

    toString

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function lancezMoi(){
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.random(3)
-    console.log(tablele[1])
-    container.style.textAlign = tablele[matchrer]
+    console.log(tablele[1].toString())
+    container.style.textAlign = tablele[matchrer].toString()
 }
 
 setInterval(lancezMoi, 100)

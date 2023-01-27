625080bbcaed1ac12943969ee1ca5586ba3e63d2
Clément Legrix <77398364+Ikon-design@users.noreply.github.com>
2023-01-27 12:06:39 +0100

    test sound

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let rick = document.getElementById("rick")
 let tablele = ["left", "center", "right"]
 
 function lancezMoi(){
+    let sound = document.createElement("audio")
+    sound.src = "./sound.mp3"
     text.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     container.style.backgroundColor = '#'+(Math.random()*0xFFFFFF<<0).toString(16)
     let matchrer = Math.floor(Math.random() * 3)
@@ -12,7 +14,7 @@ function lancezMoi(){
     console.log("Rotate(" + Math.floor(Math.random() * 360) + "deg)")
     rick.style.transform = "Rotate(" + Math.floor(Math.random() * 360) + "deg)"
     text.style.fontSize = Math.floor(Math.random() * 250) + "px"
-
+    document.body.insertBefore(sound, text)
 }
 
 setInterval(lancezMoi, 100)
